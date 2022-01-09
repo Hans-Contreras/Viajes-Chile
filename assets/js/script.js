ffe3154b145e159be2a9fc1df6b1abe134d4d738
Hans-Contreras <97134978+Hans-Contreras@users.noreply.github.com>
2022-01-08 21:09:45 -0300

    Actualización de script con funciones tooltip y cambio de color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//Cambio de color del navbar al bajar en la página//
+//Cambia de color del navbar al bajar al fondo de la página//
 window.onscroll = function (e) {  
     var element = document.getElementById("menu");
     if(scrollY >= 400){
@@ -10,7 +10,7 @@ window.onscroll = function (e) {
     }
     } 
 
-// Smooth scroll//
+//Smooth scroll//
     $(function(){
         $("a").click(function(event){
           if (this.hash !== "") {
@@ -26,3 +26,15 @@ window.onscroll = function (e) {
         $('[data-toggle="popover"]').popover();
         });
 
+//Ocultar el texto informativo de todas las card al hacer click en cualquier titulo de las mismas//
+$(function() {
+    $("[data-toggle='tooltip']").tooltip(); 
+    $(".card-title").click(function() { 
+        $(".card-text").fadeToggle();
+    });
+
+//Cambiar el color de fondo de la sección "Quienes somos" al hacer click en el título//
+    $(".color-qs").on("click", function() { 
+        $("#quienes-somos").css("background-color", "#0600CA");
+    }, );
+    });
